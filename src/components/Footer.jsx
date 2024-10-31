b53add02cae8ef414f50aa57a7f5ee12fe078771
edward conte <eac267@cornell.edu>
2024-10-31 00:34:48 -0400

    removed speaking from nav and changed uses to skills

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -25,7 +25,7 @@ export function Footer() {
                 <NavLink href="/about">About</NavLink>
                 <NavLink href="/projects">Projects</NavLink>
                 <NavLink href="/articles">Articles</NavLink>
-                <NavLink href="/uses">Uses</NavLink>
+                <NavLink href="/uses">Skills</NavLink>
               </div>
               <p className="text-sm text-zinc-400 dark:text-zinc-500">
                 &copy; {new Date().getFullYear()} Edward Conte. All rights
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -117,8 +117,7 @@ function MobileNavigation(props) {
             <MobileNavItem href="/about">About</MobileNavItem>
             <MobileNavItem href="/articles">Articles</MobileNavItem>
             <MobileNavItem href="/projects">Projects</MobileNavItem>
-            <MobileNavItem href="/speaking">Speaking</MobileNavItem>
-            <MobileNavItem href="/uses">Uses</MobileNavItem>
+            <MobileNavItem href="/uses">Skills</MobileNavItem>
           </ul>
         </nav>
       </PopoverPanel>
@@ -157,7 +156,6 @@ function DesktopNavigation(props) {
         <NavItem href="/about">About</NavItem>
         <NavItem href="/articles">Articles</NavItem>
         <NavItem href="/projects">Projects</NavItem>
-        {/*<NavItem href="/speaking">Speaking</NavItem>*/}
         <NavItem href="/uses">Skills</NavItem>
       </ul>
     </nav>
