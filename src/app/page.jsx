474b59ff4537ae7a9eed15a9f8b713b29a3af749
edward conte <eac267@cornell.edu>
2025-02-17 17:38:17 -0500

    included od in main page images

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -21,7 +21,8 @@ import image2 from '@/images/photos/image-2.jpg'
 import image3 from '@/images/photos/image-3.jpg'
 import image5 from '@/images/photos/image-5.jpg'
 import designsys from '@/images/photos/design-sys.png'
-import handmodel from '@/images/photos/final-model.png'
+//import handmodel from '@/images/photos/final-model.png'
+import od from '@/images/photos/od.jpg'
 import { getAllArticles } from '@/lib/articles'
 import { formatDate } from '@/lib/formatDate'
 
@@ -243,7 +244,7 @@ function Photos() {
   return (
     <div className="mt-16 sm:mt-20">
       <div className="-my-4 flex justify-center gap-5 overflow-hidden py-4 sm:gap-8">
-        {[image5, image3, designsys, handmodel, image2].map((image, imageIndex) => (
+        {[image5, image3, designsys, od, image2].map((image, imageIndex) => (
           <div
             key={image.src}
             className={clsx(
